8ed0dbff42b749bf98bb68d42ca2bef30e2f186b
Gorka <gorka@Mac-mini-de-Gorka.local>
2017-03-11 20:24:52 +0100

    Update changes from Visual Studio Code

--- 00001_AngularJS_Auth0/angular-auth/server/server.js
+++ 00001_AngularJS_Auth0/angular-auth/server/server.js
@@ -6,16 +6,17 @@ var cors = require('cors');
 app.use(cors());
 
 var authCheck = jwt({
-  secret: new Buffer('YOUR_AUTH0_SECRET', 'base64'),
+  //secret: new Buffer('YOUR_AUTH0_SECRET', 'base64'),
+  secret: new Buffer('YOUR_AUTH0_SECRET'),
   audience: 'YOUR_AUTH0_CLIENT_ID'
 });
 
 app.get('/api/public', function(req, res) {
-  res.json({ message: "Hello from a public endpoint! You don't need to be authenticated to see this." });
+  res.json({ message: "Información pública accesible para todos." });
 });
 
 app.get('/api/private', authCheck, function(req, res) {
-  res.json({ message: "Hello from a private endpoint! You DO need to be authenticated to see this." });
+  res.json({ message: "Información solamente USUARIOS LOGUEADOS." });
 });
 
 app.listen(3001);
