665462acb090cebc0662561eac2815aab83f1714
Jusue <Gorka.Jusue@thomsonreuters.com>
2017-03-29 14:36:48 +0200

    Sesion 84. Subscribe al servicio

--- 00002_Angular2/ejercicios/s006/SpotiApp/src/app/services/spotify.service.ts
+++ 00002_Angular2/ejercicios/s006/SpotiApp/src/app/services/spotify.service.ts
@@ -8,15 +8,20 @@ export class SpotifyService {
   artistas:any[] = [];
   urlBusqueda:string = "https://api.spotify.com/v1/search";
 
-  constructor( private http:Http ) { }
+  constructor( private http:Http ) { 
+
+  }
 
   getArtistas( termino:string ){
-    let query = `q=${ termino }&type=artist`;
+    let query = `?q=${ termino }&type=artist`;
     let url = this.urlBusqueda + query;
 
     //Observable
     return this.http.get( url ).map(res => {
-      console.log(res);
+      //console.log(res.json().artists.items);
+      this.artistas = res.json().artists.items;
+      console.log(this.artistas);
+      return this.artistas;
     });
   }
 
