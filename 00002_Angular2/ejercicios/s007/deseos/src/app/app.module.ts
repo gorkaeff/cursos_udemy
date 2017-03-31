6fb659082facd02e4b05cef7ebd988ddcc9f60d1
Gorka <gorka@Mac-mini-de-Gorka.local>
2017-03-31 19:21:36 +0200

    Sesion 98. Terminados y pendientes

--- 00002_Angular2/ejercicios/s007/deseos/src/app/app.module.ts
+++ 00002_Angular2/ejercicios/s007/deseos/src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
+import { PendientesComponent } from '../pages/pendientes/pendientes.component';
+import { TerminadosComponent } from '../pages/terminados/terminados.component';
+
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -12,9 +12,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
+    PendientesComponent,
+    TerminadosComponent,
     TabsPage
   ],
   imports: [
@@ -23,9 +22,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
+    PendientesComponent,
+    TerminadosComponent,
     TabsPage
   ],
   providers: [
