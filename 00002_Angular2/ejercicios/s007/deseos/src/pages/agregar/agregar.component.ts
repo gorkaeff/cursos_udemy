20a88fecadbc9ed41c33ee57535c7eb87d00aba3
Jusue <Gorka.Jusue@thomsonreuters.com>
2017-04-04 16:33:36 +0200

    Sesion 105. Mejoras agregar y pendientes

--- 00002_Angular2/ejercicios/s007/deseos/src/pages/agregar/agregar.component.ts
+++ 00002_Angular2/ejercicios/s007/deseos/src/pages/agregar/agregar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Lista, ListaItem } from '../../app/clases/index';
+import { AlertController, NavController } from 'ionic-angular';
+import { ListaDeseosService } from '../../app/services/lista-deseos.service';
 
 @Component({
     selector: 'app-agregar',
@@ -7,11 +9,15 @@ import { Lista, ListaItem } from '../../app/clases/index';
 })
 export class AgregarComponent implements OnInit {
 
-    nombreLista:string;
+    nombreLista:string = "";
     nombreItem:string = "";
     items:ListaItem[] = [];
 
-    constructor() { }
+    constructor(
+        public alertCtrl:AlertController,
+        public navCtrl:NavController,
+        public _listaDeseos:ListaDeseosService
+    ) { }
 
     ngOnInit() { }
 
@@ -28,5 +34,23 @@ export class AgregarComponent implements OnInit {
     
     borrarItem( idx:number){
         this.items.splice(idx, 1);
+    }
+
+    guardarLista(){
+        if (this.nombreLista.length == 0){
+            let alert = this.alertCtrl.create({
+                title: 'Nombre de la lista',
+                subTitle: 'El nombre de la lista es necesaria!',
+                buttons: ['OK']
+            });
+            alert.present();
+            return;
+        }
+
+        let lista = new Lista(this.nombreLista);
+        lista.items = this.items;
+        this._listaDeseos.listas.push(lista);
+
+        this.navCtrl.pop();
     }
 }
