d9c80de1b4c1f57241f3f3d05ab925660905c655
Gorka <gorka@Mac-mini-de-Gorka.local>
2017-04-18 19:46:36 +0200

    Sesion 156. Validators

--- 00002_Angular2/ejercicios/s010/formularios/src/app/components/data/data.component.ts
+++ 00002_Angular2/ejercicios/s010/formularios/src/app/components/data/data.component.ts
@@ -12,9 +12,12 @@ export class DataComponent {
 
   constructor(  ) {
     this.forma = new FormGroup({
-      'nombre': new FormControl('Gorka'),
-      'apellido': new FormControl(),
-      'correo': new FormControl()
+      'nombre': new FormControl('', Validators.required),
+      'apellido': new FormControl('', Validators.required),
+      'correo': new FormControl('', [
+        Validators.required,
+        Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")
+      ])
     });
   }
 
