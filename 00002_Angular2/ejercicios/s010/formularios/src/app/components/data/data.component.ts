0e15d87ab152f39958b926be97626cf2392a2e60
Gorka <gorka@Mac-mini-de-Gorka.local>
2017-04-18 19:34:24 +0200

    Sesion 155 FormGroup

--- 00002_Angular2/ejercicios/s010/formularios/src/app/components/data/data.component.ts
+++ 00002_Angular2/ejercicios/s010/formularios/src/app/components/data/data.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-data',
   templateUrl: './data.component.html',
   styles: []
 })
-export class DataComponent implements OnInit {
+export class DataComponent {
 
-  constructor() { }
+  forma:FormGroup;
 
-  ngOnInit() {
+  constructor(  ) {
+    this.forma = new FormGroup({
+      'nombre': new FormControl('Gorka'),
+      'apellido': new FormControl(),
+      'correo': new FormControl()
+    });
+  }
+
+  guardarCambios(){
+    console.log(this.forma.value);
   }
 
 }
