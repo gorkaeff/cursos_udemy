e10755ac548059eae32c1df010e748ad65346330
Gorka <gorka@Mac-mini-de-Gorka.local>
2017-05-02 20:15:16 +0200

    Aplicacion completa PeliculasApp

--- 00002_Angular2/ejercicios/s013/PeliculasApp/src/app/app.component.ts
+++ 00002_Angular2/ejercicios/s013/PeliculasApp/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+// import { PeliculasService } from "./services/peliculas.service";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+
+
+  constructor(){
+
+    // this._ps.buscarPelicula("wall-e")
+    //   .subscribe( data=>console.log(data) )
+
+    // this._ps.getPopulares()
+    //       .subscribe(
+    //         data => console.log(data)
+    //       )
+
+  }
+
+
+
 }
