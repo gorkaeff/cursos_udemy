c5f09cb854bc30d2ebc0d8f8c0a9dca9c6b92649
Jusue <Gorka.Jusue@thomsonreuters.com>
2017-05-08 12:17:32 +0200

    Inicio Proyecto YoutubeApp - Leccion 219

--- 00002_Angular2/ejercicios/s015/youtubeapp/src/app/app.module.ts
+++ 00002_Angular2/ejercicios/s015/youtubeapp/src/app/app.module.ts
@@ -5,6 +5,9 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+//youtube API KEY
+//AIzaSyAOfTvX-CuuLxOjTA4p-0D2MGrEGa6ondU
+
 @NgModule({
   declarations: [
     AppComponent
