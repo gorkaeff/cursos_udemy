af6dac2afeb36dccaa6ea3edf27496ff5f6ce23c
Gorka <gorka@Mac-mini-de-Gorka.local>
2017-05-10 18:09:06 +0200

    Leccion 239. Componentes y rutas de app

--- 00002_Angular2/ejercicios/s018/firebasefotos/src/app/app.module.ts
+++ 00002_Angular2/ejercicios/s018/firebasefotos/src/app/app.module.ts
@@ -3,16 +3,24 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+//routes
+import { APP_ROUTING } from "./app.routes";
+
 import { AppComponent } from './app.component';
+import { CargaComponent } from './components/carga/carga.component';
+import { FotosComponent } from './components/fotos/fotos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CargaComponent,
+    FotosComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ 00002_Angular2/ejercicios/s018/firebasefotos/src/app/components/carga/carga.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-carga',
+  templateUrl: './carga.component.html'
+})
+export class CargaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
