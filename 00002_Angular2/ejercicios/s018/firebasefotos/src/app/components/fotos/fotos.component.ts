1c08d05202edf63d904abeafdd06a2fa5b86b6c1
Gorka <gorka@Mac-mini-de-Gorka.local>
2017-05-12 23:56:41 +0200

    Subir imagenes a firebase. Fin

--- 00002_Angular2/ejercicios/s018/firebasefotos/src/app/components/fotos/fotos.component.ts
+++ 00002_Angular2/ejercicios/s018/firebasefotos/src/app/components/fotos/fotos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CargaImagenesService } from "../../services/carga-imagenes.service";
+import { FirebaseListObservable } from "angularfire2";
 
 @Component({
   selector: 'app-fotos',
@@ -7,7 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FotosComponent implements OnInit {
 
-  constructor() { }
+  imagenes: FirebaseListObservable<any[]>;
+
+  constructor( public _cargaImagenes:CargaImagenesService) {
+    this.imagenes = this._cargaImagenes.listaUltimasImagenes(10);
+  }
 
   ngOnInit() {
   }
